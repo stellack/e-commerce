b7897b363cb6c3725c50bf079bc329730a6b979d
stellack <chemutaistella01@gmail.com>
2023-04-30 14:16:00 +0300

    modified routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { CartComponent } from './pages/cart/cart.component';
+import { SaleComponent } from './pages/sale/sale.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'products',
+    component: HomeComponent
+  },
+  {
+    path: 'cart',
+    component: CartComponent
+  },
+  {
+    path: 'sale',
+    component: SaleComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
